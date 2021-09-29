de2237391932d578f5a85f1d92842967f5bfdc54
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-09-29 17:45:30 +0600

    img added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 const About = () => {
     return (
         <div>
+            <img src="/src/images/sharif.jpg" alt="" />
             <h3>This is about page</h3>
         </div>
     );
