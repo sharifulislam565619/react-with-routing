62bca5b1886e4429a9a26ebabda931719005a634
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-09-29 18:04:41 +0600

    bug fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import img from '/src/images/sharif.jpg'
+
 
 const About = () => {
     return (
         <div>
-            <img src={img} alt="" />
+
             <h3>This is about page</h3>
             <p>about coming soon...</p>
         </div>
